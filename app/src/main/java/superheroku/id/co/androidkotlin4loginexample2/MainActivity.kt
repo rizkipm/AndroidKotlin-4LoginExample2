bda9ab88226e4df2646b3e5b5c88e308cff4f41a
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 21:13:29 +0700

    add

---
+++ app/src/main/java/superheroku/id/co/androidkotlin4loginexample2/MainActivity.kt
@@ -0,0 +1,12 @@
+package superheroku.id.co.androidkotlin4loginexample2
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
